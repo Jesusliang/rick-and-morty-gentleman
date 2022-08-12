c5426ca44a04282e4ed000db701c932913be7d45
Jesus Liang <jesusliang96@gmail.com>
2022-08-12 22:23:59 +0200

    added modal

---
+++ src/components/triviaResultModal/TriviaResultModal.tsx
@@ -0,0 +1,75 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  ModalProps,
+  Box,
+  Text
+} from "@chakra-ui/react"
+import React from "react"
+import { RiErrorWarningFill, RiCheckboxCircleFill } from "react-icons/ri"
+import { Character, TriviaOption } from "src/models"
+
+export interface TriviaResultModalProps extends Omit<ModalProps, "children"> {
+  onClose: () => void
+  correct: boolean
+  character: Character
+  correctOption: TriviaOption
+}
+
+const TriviaResultModal: React.FC<TriviaResultModalProps> = ({
+  correct,
+  onClose,
+  character,
+  correctOption,
+  ...props
+}) => {
+  return (
+    <Modal size="xs" onClose={() => {}} {...props}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalBody>
+          <Box
+            textAlign="center"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+            fontSize="2rem"
+            fontWeight="500"
+          >
+            {correct ? (
+              <>
+                <RiCheckboxCircleFill fontSize={"5rem"} color="green" />
+                <Text color="green">Correcto!</Text>
+              </>
+            ) : (
+              <>
+                <RiErrorWarningFill fontSize={"5rem"} color="red" />
+                <Text color="red">Incorrecto!</Text>
+              </>
+            )}
+            <Text>{`${character?.name} es del planeta ${correctOption?.name}!`}</Text>
+          </Box>
+        </ModalBody>
+        <ModalFooter justifyContent="center" marginTop="2rem">
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={onClose}
+            textAlign="center"
+            marginRight={0}
+          >
+            Reiniciar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+export default TriviaResultModal
